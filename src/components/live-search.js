5a318d569f55ff81be81027a6c38357184ef3f8c
vcordero07 <vcordero07@gmail.com>
2017-12-08 23:06:07 -0500

    wip

--- src/components/live-search.js
+++ src/components/live-search.js
@@ -3,11 +3,33 @@ import React from 'react';
 import SearchForm from './search-form';
 import CharacterList from './character-list';
 
-export default function LiveSearch(props) {
-    return (
-        <div className="live-search">
-            <SearchForm />
-            <CharacterList characters={props.characters} />
-        </div>
-    );
+// export default function LiveSearch(props) {
+//     return (
+//         <div className="live-search">
+//             <SearchForm />
+//             <CharacterList characters={props.characters} />
+//         </div>
+//     );
+// }
+export default class LiveSearch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: ''
+        }
+    }
+
+    render() {
+        const characters = this.props.characters.filter(character =>
+            character.name.toLowerCase().includes(
+                this.state.searchTerm.toLowerCase()
+            )
+        );
+        return (
+            <div className="live-search">
+                <SearchForm onChange={searchTerm => this.setState({searchTerm})} />
+                <CharacterList characters={characters} />
+            </div>
+        );
+    }
 }
--- src/components/search-form.js
+++ src/components/search-form.js
@@ -4,7 +4,7 @@ export default function SearchForm(props) {
     return (
         <form onSubmit={e => e.preventDefault()}>
             <label htmlFor="search">Search</label>&emsp;
-            <input type="search" id="search" name="search" placeholder="Dale Cooper" />
+            <input type="search" id="search" name="search" placeholder="Dale Cooper" onChange={e => props.onChange(e.target.value)} />
         </form>
     );
 }

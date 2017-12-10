57f3421cd14ab61c33535bea0125d1f1b90e3ce2
vcordero07 <vcordero07@gmail.com>
2017-12-09 23:03:31 -0500

    wip

--- src/components/live-search.js
+++ src/components/live-search.js
@@ -3,14 +3,6 @@ import React from 'react';
 import SearchForm from './search-form';
 import CharacterList from './character-list';
 
-// export default function LiveSearch(props) {
-//     return (
-//         <div className="live-search">
-//             <SearchForm />
-//             <CharacterList characters={props.characters} />
-//         </div>
-//     );
-// }
 export default class LiveSearch extends React.Component {
     constructor(props) {
         super(props);
